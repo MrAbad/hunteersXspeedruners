cb6c7746e756b36461f3a41ae8a3ed0ac104b90e
MrAbad <MrAbad@computercraft.ru>
2020-11-26 20:16:39 +0500

    Realise v1.0

--- src/ru/mrabad/events/OnPortalTp.java
+++ src/ru/mrabad/events/OnPortalTp.java
@@ -1,11 +1,11 @@
 package ru.mrabad.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CompassMeta;
@@ -23,8 +23,9 @@ public class OnPortalTp implements Listener
     @EventHandler
     public void onPortalTp(PlayerPortalEvent e)
     {
-        if (plugin.speedrunnersList.isEmpty()) return;
         Player p = e.getPlayer();
+        if (plugin.speedrunnersList.isEmpty()) return;
+        if (e.getTo().getWorld().getName().equals("world_the_end")) return;
         for (int itemCounter = 0; itemCounter < p.getInventory().getContents().length; itemCounter++)
         {
             ItemStack item = p.getInventory().getItem(itemCounter);
@@ -33,17 +34,20 @@ public class OnPortalTp implements Listener
                 CompassMeta meta = (CompassMeta) item.getItemMeta();
                 if (meta != null && meta.hasLodestone())
                     meta.setLodestoneTracked(false);
+                    Location mls;
                     if (e.getTo().getWorld() == meta.getLodestone().getWorld())
                     {
                         meta.setLodestone(meta.getLodestone());
-                    } else if (e.getTo().getWorld() == Bukkit.getWorlds().get(0))
+                    } else if (e.getTo().getWorld().getName().equals("world"))
                     {
-                        meta.getLodestone().setWorld(e.getTo().getWorld());
-                        meta.setLodestone(meta.getLodestone().multiply(8));
-                    } else if (e.getTo().getWorld() == Bukkit.getWorlds().get(1))
+                        mls = meta.getLodestone();
+                        mls.setWorld(Bukkit.getWorld("world"));
+                        meta.setLodestone(mls.multiply(8));
+                    } else if (e.getTo().getWorld().getName().equals("world_nether"))
                     {
-                        meta.getLodestone().setWorld(e.getTo().getWorld());
-                        meta.setLodestone(meta.getLodestone().multiply(1. / 8));
+                        mls = meta.getLodestone();
+                        mls.setWorld(Bukkit.getWorld("world_nether"));
+                        meta.setLodestone(mls.multiply(1./8));
                     }
                     item.setItemMeta(meta);
                     p.getInventory().setItem(itemCounter, item);
