e7538c8a89888799f8b8e9610516b861663049fe
MrAbad <MrAbad@computercraft.ru>
2020-11-26 10:04:51 +0500

    Init

---
+++ src/ru/mrabad/main/HxS.java
@@ -0,0 +1,18 @@
+package ru.mrabad.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HxS extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
